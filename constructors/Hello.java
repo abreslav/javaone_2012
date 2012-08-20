45527091899adf13c85923f799351c62a4debfda
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-08-20 11:56:59 +0400

    Initial

---
+++ constructors/Hello.java
@@ -0,0 +1,12 @@
+
+
+
+    public class Hello {
+        String name;
+
+        void sayHello() {
+            System.out.println("Hi, I am " + name);
+        }
+    }
+
+
---
+++ constructors/hello.kt
@@ -0,0 +1,2 @@
+
+   class HelloK(val name: String)
